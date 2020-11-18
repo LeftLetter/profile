822a3be57398821844cd6b09df54d2ab5407ae68
LeftLetter <zmsh_white@yahoo.co.jp>
2020-11-18 12:00:43 +0000

    追加：ストーリー

---
+++ components/atoms/CommonText.stories.js
@@ -0,0 +1,34 @@
+import CommonText from '@/components/atoms/CommonText'
+
+export default {
+  title: 'components/atoms/CommonText',
+}
+
+const sampleText = 'サンプルテキストです。'
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const Template = (args, { argTypes }) => ({
+  components: { CommonText },
+  props: Object.keys(argTypes),
+  template: '<CommonText :text="text" :color="color" :size="size" />',
+})
+
+export const Small = Template.bind({})
+Small.args = { text: sampleText, size: 's' }
+Small.storyName = '小さい文字'
+
+export const Middle = Template.bind({})
+Middle.args = { text: sampleText }
+Middle.storyName = '中くらいの文字'
+
+export const Large = Template.bind({})
+Large.args = { text: sampleText, size: 'l' }
+Large.storyName = '大きい文字'
+
+export const Warning = Template.bind({})
+Warning.args = { text: sampleText, color: 'warning' }
+Warning.storyName = 'ワーニング'
+
+export const Error = Template.bind({})
+Error.args = { text: sampleText, color: 'error' }
+Error.storyName = 'エラー'
