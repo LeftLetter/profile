efec6b4654c4288f3a27848243144073fa629419
LeftLetter <zmsh_white@yahoo.co.jp>
2020-11-23 08:19:58 +0000

    追加：ユーザ表示

---
+++ components/molecules/UserCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <CommonImage :src="userIcon" shape="circle"></CommonImage>
+    <CommonTitle text="@LeftLetter" :level="1" visual-level="h4" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import CommonImage from '@/components/atoms/CommonImage.vue'
+import CommonTitle from '@/components/atoms/CommonTitle.vue'
+import UserImage from '@/assets/icon_1000x1000.png'
+
+export default Vue.extend({
+  components: {
+    CommonImage,
+    CommonTitle,
+  },
+  data() {
+    return {
+      userIcon: UserImage,
+    }
+  },
+})
+</script>
+
+<style scoped></style>
---
+++ components/organisms/UserArea.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="user-card">
+    <UserCard />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import UserCard from '@/components/molecules/UserCard.vue'
+
+export default Vue.extend({
+  components: {
+    UserCard,
+  },
+})
+</script>
+
+<style scoped>
+.user-card {
+  display: grid;
+  place-items: center;
+}
+</style>
